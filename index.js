ff701f2520fbd6a222a3f97f4018f439973a092a
Gopalakrishnan Vadivel <gopalarthi2014@gmail.com>
2024-06-17 10:59:27 +0530

    Modified code v1.0.8

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const jwt = require("jsonwebtoken");
 const connectionString = "mongodb+srv://gopalarthi2014:ZpAhpWClYp7Vtoiy@inamapp.sgxgwjd.mongodb.net/";
 //const dbURI = process.env.MONGODB_URI;
 
-//mongoose.connect("mongodb://localhost:27017", {
-mongoose.connect(connectionString, {
+mongoose.connect("mongodb://localhost:27017", {
+//mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
@@ -134,7 +134,7 @@ const generateSecretKey = () => {
 const secretKey = generateSecretKey();
 
 //code to login
-app.post("/api/signin", async (req, res) => {
+app.post("/signin", async (req, res) => {
   try {
     //console.log("gopal")
     const { identifier, password } = req.body;
